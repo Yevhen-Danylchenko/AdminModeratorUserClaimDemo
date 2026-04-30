8166565424975cef0f5feab9fa52936671421b8f
Yevhen <yevhen_danylchenko@ukr.net>
2026-04-30 15:23:46 +0300

    Added Views and edit controllers

--- AdminModeratorUserClaimDemo/Controllers/HomeController.cs
+++ AdminModeratorUserClaimDemo/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
+using AdminModeratorUserClaimDemo.Data;
 using AdminModeratorUserClaimDemo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AdminModeratorUserClaimDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
